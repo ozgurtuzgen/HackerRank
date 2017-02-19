0914043a37aecc4fd5f4030c7d086df82e4f32a3
Ozgur Tuzgen <ozgurtuzgen@hotmail.com>
2017-02-19 21:17:39 +0300

    day6 - 30 days of code

--- HackerRank/30DaysOfCode/Day6_LetsReview.cs
+++ HackerRank/30DaysOfCode/Day6_LetsReview.cs
@@ -10,62 +10,31 @@ namespace HackerRank._30DaysOfCode
     {
         public static void Calculate()
         {
-            int n = 10;// Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
             string evenString = string.Empty;
             string oddString = string.Empty;
-            string evenStringLast = string.Empty;
-            string oddStringLast = string.Empty;
 
             for (int i = 0; i < n; i++)
             {
-
-                var inputString = string.Empty;// Console.ReadLine();
-                for (int z = 0; z < 10000; z++)
-                {
-                    inputString += "a";
-                }
-
-                Console.WriteLine(DateTime.Now);
-
+                var inputString = Console.ReadLine();
                 oddString = string.Empty;
                 evenString = string.Empty;
-                oddStringLast = string.Empty;
-                evenStringLast = string.Empty;
-                string lastChar = string.Empty;
-                var inputLength = inputString.Length;
-                int remainder;
 
-                int midIndex = Math.DivRem(inputLength, 2, out remainder);
-
-                if(remainder == 1)
+                for (int k = 0; k < inputString.Length; k = k + 2)
                 {
-                    lastChar = inputString.Last().ToString();
-                    inputString = inputString.Substring(0, inputLength - 1);
-                }
 
+                    //even
+                    evenString += inputString[k];
 
-                for (int k = 0;k< midIndex; k++)
-                {
-                    if (k % 2 == 0)
+                    // odd
+                    if (k + 1 < inputString.Length)
                     {
-                        //even
-                        evenString += inputString.ElementAt(k);
-                        oddStringLast = inputString.ElementAt(inputString.Length - 1 - k) + oddStringLast;
-
+                        oddString += inputString[k + 1];
                     }
-                    else
-                    {
-                        // odd
-                        oddString += inputString.ElementAt(k);
-                        evenStringLast = inputString.ElementAt(inputString.Length - 1 - k) + evenStringLast;
 
-                    }
                 }
-
-                //Console.WriteLine(evenString + evenStringLast + lastChar + " " + oddString+oddStringLast);
+                Console.WriteLine(evenString + " " + oddString);
             }
-            Console.WriteLine(DateTime.Now);
-            Console.ReadLine();
         }
     }
 }
