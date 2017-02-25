5aec4e5e17db7fd2d10b8835458bc1618e2fd09f
Ozgur Tuzgen <ozgurtuzgen@hotmail.com>
2017-02-25 14:44:54 +0300

    30 days of code continues

---
+++ HackerRank/30DaysOfCode/Day12_Inheritance.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank._30DaysOfCode
+{
+    public static class Day12_Inheritance
+    {
+        public static void Calculate()
+        {
+            string[] inputs = Console.ReadLine().Split();
+            string firstName = inputs[0];
+            string lastName = inputs[1];
+            int id = Convert.ToInt32(inputs[2]);
+            int numScores = Convert.ToInt32(Console.ReadLine());
+            inputs = Console.ReadLine().Split();
+            int[] scores = new int[numScores];
+            for (int i = 0; i < numScores; i++)
+            {
+                scores[i] = Convert.ToInt32(inputs[i]);
+            }
+
+            Student s = new Student(firstName, lastName, id, scores);
+            s.printPerson();
+            Console.WriteLine("Grade: " + s.Calculate() + "\n");
+        }
+    }
+
+    class Student : Person
+    {
+        private int[] testScores;
+
+        public Student(string firstName, string lastName, int id, int[] scores)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+            this.testScores = scores;
+        }
+
+        public char Calculate()
+        {
+            var average = this.testScores.Average();
+            if (average>=90)
+            {
+                return 'O';
+            }
+            else if (average >= 80)
+            {
+                return 'E';
+            }
+            else if (average >= 70)
+            {
+                return 'A';
+            }
+            else if (average >= 55)
+            {
+                return 'P';
+            }
+            else if (average >= 40)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'T';
+            }
+        }
+
+        /*	
+        *   Class Constructor
+        *   
+        *   Parameters: 
+        *   firstName - A string denoting the Person's first name.
+        *   lastName - A string denoting the Person's last name.
+        *   id - An integer denoting the Person's ID number.
+        *   scores - An array of integers denoting the Person's test scores.
+        */
+        // Write your constructor here
+
+        /*	
+        *   Method Name: Calculate
+        *   Return: A character denoting the grade.
+        */
+        // Write your method here
+    }
+
+    class Person
+    {
+        protected string firstName;
+        protected string lastName;
+        protected int id;
+
+        public Person() { }
+        public Person(string firstName, string lastName, int identification)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = identification;
+        }
+        public void printPerson()
+        {
+            Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+        }
+    }
+
+}
---
+++ HackerRank/30DaysOfCode/Day13_AbstractClasses.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank._30DaysOfCode
+{
+    public static class Day13_AbstractClasses
+    {
+        public static void Calculate()
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }        
+    }
+
+    class MyBook : Book
+    {
+        int price;
+
+        public MyBook(string title, string author, int price):base(title, author)
+        {
+            this.title = title;
+            this.author = author;
+            this.price = price;            
+        }
+
+        public override void display()
+        {
+            Console.WriteLine("Title: {0}", this.title);
+            Console.WriteLine("Author: {0}", this.author);
+            Console.WriteLine("Price: {0}", this.price);
+        }
+    }
+
+
+    abstract class Book
+    {
+
+        protected String title;
+        protected String author;
+
+        public Book(String t, String a)
+        {
+            title = t;
+            author = a;
+        }
+        public abstract void display();
+    }
+}
